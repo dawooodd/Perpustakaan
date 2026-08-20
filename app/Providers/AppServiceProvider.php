cfb0849e377c79417979499584cb2c1f14161340
achmad nasich <muchammadnasich896@gmail.com>
2026-08-20 16:52:22 +0700

    er

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,18 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // --- BYPASS CACHE WASMER ---
+        // Kode ini akan menimpa cache yang terkunci secara paksa
+        config([
+            'database.default' => 'mysql',
+            'database.connections.mysql.host' => 'db.fr-roub1.bengt.wasmernet.com',
+            'database.connections.mysql.port' => '20184',
+            'database.connections.mysql.database' => 'db_9b1ae70a',
+            'database.connections.mysql.username' => 'user_d2717e1e',
+            'database.connections.mysql.password' => 'Mpw_Ls293ey8AINanXMSzKq57CTBDUAkFtqU',
+            
+            // Bypass session agar tidak error saat baca database
+            'session.driver' => 'cookie', 
+        ]);
     }
 }

5dc78983a5a72ca210090afbb0678b5406d3290b
achmad nasich <muchammadnasich896@gmail.com>
2026-08-22 17:58:31 +0700

    provider configurations

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,23 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // --- BYPASS CACHE WASMER ---
-        // Kode ini akan menimpa cache yang terkunci secara paksa
-        config([
-            'database.default' => 'mysql',
-            'database.connections.mysql.host' => 'db.fr-roub1.bengt.wasmernet.com',
-            'database.connections.mysql.port' => '20184',
-            'database.connections.mysql.database' => 'db_9b1ae70a',
-            'database.connections.mysql.username' => 'user_d2717e1e',
-            'database.connections.mysql.password' => 'Mpw_Ls293ey8AINanXMSzKq57CTBDUAkFtqU',
-            
-            // Bypass session agar tidak error saat baca database
-            'session.driver' => 'cookie', 
-            
-            // PAKSA MENGGUNAKAN SSL UNTUK AIVEN CLOUD
-            'database.connections.mysql.options' => extension_loaded('pdo_mysql') ? [
-                \PDO::MYSQL_ATTR_SSL_CA => storage_path('ca.pem'),
-            ] : [],
-        ]);
+        //
     }
 }
